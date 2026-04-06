5192dc2b044902dc41fbae02dddbe6530df91314
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-04-06 12:54:20 +0200

    final

--- views/dashboard.php
+++ views/dashboard.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Performances du Club - Dashboard</title>
-    <link rel="stylesheet" href="views/style.css">
-    <script src="views/script.js" defer></script>
+    <link rel="stylesheet" href="assets/style.css">
+    <script src="assets/script.js" defer></script>
 </head>
 
 <body>
